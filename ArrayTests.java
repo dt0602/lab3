cb8d0f72021cf42b265c22e4a5277149b028cd4f
dt0602 <diya02t@gmail.com>
2023-01-25 16:22:57 -0800

    Lab 3 debugging

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,18 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testPlace1() {
+    int[] i1 = {4, 5, 6, 7 , 8, 9};
+    ArrayExamples.reverseInPlace(i1);
+    assertArrayEquals(new int[]{9, 8, 7, 6, 5, 4}, i1);
+  }
+
+  @Test
+  public void testReversed1() {
+    int[] input1 = {4, 5, 6, 7 , 8, 9};
+    assertArrayEquals(new int[]{9, 8, 7, 6, 5, 4}, ArrayExamples.reversed(input1));
+  }
+
 }
